a02bcb4b3d6efae7bec59c36df49398427071661
Pierre-Philippe Emond <emondpph@gmail.com>
2022-05-17 13:44:34 -0400

    name file

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,6 +1,6 @@
 const mix = require('laravel-mix');
 mix.js('index.js', 'dist/browser.js');
-mix.js('test/test.js', 'dist/browser-test.js');
+mix.js('test/browser.js', 'dist/browser-test.js');
 mix.babel('dist/browser.js', 'dist/browser-es5.js');
 mix.babel('dist/browser-test.js', 'dist/browser-test-es5.js');
 mix.webpackConfig({
